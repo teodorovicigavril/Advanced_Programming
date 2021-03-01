facaaf9ebf2ae93a80df66773ec1164d9aef2539
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-03-01 12:58:32 +0200

    Add files via upload

---
+++ lab2/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.company","l":"Destination"},{"p":"com.company","l":"Factory"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"Problem"},{"p":"com.company","l":"Solution"},{"p":"com.company","l":"Source"},{"p":"com.company","l":"Warehouse"}];updateSearchResults();
