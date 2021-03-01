f707c8a10b3f100a77ce5983920e9c15a1b06347
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-03-01 12:29:40 +0200

    Update Main.java

--- lab2/Main.java
+++ lab2/Main.java
@@ -33,22 +33,42 @@ public class Main {
         destinations.add(D1);
         destinations.add(D2);
 
-       // Problem P1 = new Problem(sources,destinations);
-       // System.out.println(P1);
-
-       // P1.printingInstance();
-       // P1.addSource(S3);
-       // P1.addDestination(D3);
-       // P1.printingInstance();
 
         Solution Sol = new Solution(sources, destinations);
         Sol.printingInstance();
         Sol.feasibleSolution();
         System.out.println("Total cost: " + Sol.getCostTotal());
-        Sol.vogelMethod();
+
+
+        Factory S1_0 = new Factory(10,"S1","Factory",0);
+        Warehouse S1_1 = new Warehouse(35,"S2", "Factory", 1);
+        Warehouse S1_2 = new Warehouse(25,"S3", "Warehouse", 2);
+        Warehouse S1_3 = new Warehouse(25,"S4", "Warehouse", 3);
+
+        Destination D1_0 = new Destination(20,"D1",0);
+        Destination D1_1 = new Destination(25,"D2",1);
+        Destination D1_2 = new Destination(25,"D3",2);
+        //Destination D1_3 = new Destination(25,"D3",2);
+        Destination D1_3 = new Destination(25,"D4",3);
+
+        ArrayList<Source> sources1 = new ArrayList<Source>();
+        ArrayList<Destination> destinations1 = new ArrayList<Destination>();
+
+        sources1.add(S1_0);
+        sources1.add(S1_1);
+        sources1.add(S1_2);
+
+        destinations1.add(D1_0);
+        destinations1.add(D1_1);
+        destinations1.add(D1_2);
+
+        Solution Sol2 = new Solution(sources1,destinations1);
+
+        Sol2.printingInstance();
+        Sol2.vogelMethod();
         System.out.println("");
-        //Sol.printingInstance();
-        System.out.println("Cost total: " + Sol.getCostTotal());
+        //Sol2.printingInstance();
+        System.out.println("Cost total: " + Sol2.getCostTotal());
     }
 
 }
