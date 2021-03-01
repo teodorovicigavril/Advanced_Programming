3ca8a8145f0e18333533862c0bda38ad0a1f2cde
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-03-01 09:45:23 +0200

    Delete company directory

--- company/Clasifiable.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Clasifiable {
-    //int rank = 1;
-    int getRank();
-}
--- company/Visitable.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.time.LocalTime;
-
-public interface Visitable {
-    LocalTime getOpeningTime();
-    LocalTime getClosingTime();
-}
