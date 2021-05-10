ec2d531f541ebe9e94bdc0aff271d5ac49b903e1
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-05-10 09:44:50 +0300

    Add files via upload

---
+++ demo1111/src/main/java/com/example/demo/Person.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
